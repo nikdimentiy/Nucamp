a376568b00bbe2363d7d8f12592bafc1391d295d
nikdimentiy <nikdimentiy@yahoo.com>
2022-06-28 21:50:41 -0700

    Update decimal_to_bin.py

--- Python Algorithms/decimal_to_bin.py
+++ Python Algorithms/decimal_to_bin.py
@@ -1,4 +1,7 @@
 # convert decimal numbers to binary numbers
+from unittest import result
+
+
 def decimal_to_binary(decimal_number):
     binary_number = ''
     while decimal_number > 0:
@@ -6,5 +9,8 @@ def decimal_to_binary(decimal_number):
         decimal_number = decimal_number // 2
     return binary_number
 
+
 # Alternative method - built-in function
-bin(decimal_number)
+# bin(decimal_number)
+result = decimal_to_binary(150)
+print(result)
