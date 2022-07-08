6b8c329eac36907f034047c8cf064ecd6b6c48c6
nikdimentiy <nikdimentiy@yahoo.com>
2022-07-07 21:17:38 -0700

    Add files via upload

---
+++ Python Algorithms/binary_search.py
@@ -0,0 +1,36 @@
+# Binary Search - time complexity: O(log(n))
+# NOTE: This works only for sorted arrays
+# Iterative method
+def binary_search(l, value):
+    LOW = 0
+    TOP = len(l)-1
+    while LOW <= TOP:
+        MID = (LOW + TOP) // 2
+        if l[MID] > value:
+            TOP = MID-1
+        elif l[MID] < value:
+            LOW = MID+1
+        else:
+            return MID
+    return -1
+
+# Recursive method
+
+
+def binary_search(l, value, LOW=0, TOP=-1):
+    if not l:
+        return -1
+    if TOP == -1:
+        TOP = len(l)-1
+    if LOW >= TOP:
+        if l[LOW] == value:
+            return LOW
+        else:
+            return -1
+    MID = (LOW + TOP) // 2
+    if l[MID] > value:
+        return binary_search(l, value, LOW, MID-1)
+    elif l[MID] < value:
+        return binary_search(l, value, MID+1, TOP)
+    else:
+        return MID
