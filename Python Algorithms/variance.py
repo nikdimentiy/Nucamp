2a9b1e43d145b6f667e53f0c5081acecdf13b4ff
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-18 20:25:15 -0700

    Update variance.py
    create doc string and add comments

--- Python Algorithms/variance.py
+++ Python Algorithms/variance.py
@@ -1,9 +1,43 @@
+"""
+This function calculates the variance of a list of numbers.
+
+Args:
+    arr (list): A list of numbers.
+
+Returns:
+    float: The variance of the list.
+"""
+
 def variance(arr):
-    """Returns the variance of a list of numbers"""
+    """
+    Calculates the variance of a list of numbers.
+
+    1. Calculate the mean of the list.
+    2. Iterate over the list and calculate the squared deviation from the mean for each number.
+    3. Sum the squared deviations.
+    4. Divide the sum of the squared deviations by the number of numbers in the list.
+
+    Args:
+        arr (list): A list of numbers.
+
+    Returns:
+        float: The variance of the list.
+    """
+
+    # Calculate the mean of the list.
     mean = mean(arr)
+
+    # Initialize the total and length variables.
     total = 0
-    length = 0
+    length = len(arr)
+
+    # Iterate over the list and calculate the squared deviation from the mean for each number.
     for i in arr:
-        total += (i - mean) ** 2
-        length += 1
+        # Calculate the squared deviation from the mean.
+        squared_deviation = (i - mean) ** 2
+
+        # Add the squared deviation to the total.
+        total += squared_deviation
+
+    # Return the variance, which is the total divided by the length.
     return total / length
