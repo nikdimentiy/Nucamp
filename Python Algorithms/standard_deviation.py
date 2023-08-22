4615e6f877825827ebdd0e8215a6a544bc96216b
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-21 23:16:49 -0700

    Update standard_deviation.py

--- Python Algorithms/standard_deviation.py
+++ Python Algorithms/standard_deviation.py
@@ -1,5 +1,27 @@
 def standard_deviation(arr, mu=None):
-    """Returns the standard deviation of a list of numbers"""
+    """
+    Returns the standard deviation of a list of numbers.
+
+    Args:
+        arr: A list of numbers.
+        mu: The mean of the list. If not provided, the mean will be calculated.
+
+    Returns:
+        The standard deviation of the list.
+
+    """
+
+    # Check if the mean was provided. If not, calculate it.
+
     if not mu:
         mu = mean(arr)
-    return sum((x - mu) ** 2 for x in arr) ** 0.5
+
+    # Calculate the squared deviations from the mean for each element in the list.
+
+    deviations = [(x - mu) ** 2 for x in arr]
+
+    # Return the square root of the sum of the squared deviations.
+
+    return sum(deviations) ** 0.5
+
+
