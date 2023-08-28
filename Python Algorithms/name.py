b81db2f323c1fc5b4b132fd60b07d4186e1bab65
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-27 23:18:38 -0700

    Update name.py

--- Python Algorithms/name.py
+++ Python Algorithms/name.py
@@ -1,8 +1,28 @@
+"""
+This program prints "Hello, my name is <argument>" for each argument passed to the program.
+
+Usage:
+    python hello_name.py <argument>
+
+Example:
+    python hello_name.py Nikey
+
+Output:
+    Hello, my name is Nikey
+"""
+
 import sys
 
-if len(sys.argv) < 2:
-    sys.exit("To few arguments👇️")
 
-for arg in sys.argv[1:]:
-	print("🍉️ Hello, my name is", arg)
+def main():
+    """The main function."""
+    if len(sys.argv) < 2:
+        sys.exit("To few arguments.")
+
+    for arg in sys.argv[1:]:
+        print("Hello, my name is", arg)
+
+
+if __name__ == "__main__":
+    main()
 
