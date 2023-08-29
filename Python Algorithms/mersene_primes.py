267f3dffd2edf29fe767b12633fd8fb464b0e5e1
nikdimentiy <nikdimentiy@yahoo.com>
2023-08-28 20:10:24 -0700

    Update mersene_primes.py

--- Python Algorithms/mersene_primes.py
+++ Python Algorithms/mersene_primes.py
@@ -1,4 +1,13 @@
 def is_prime(num: int):
+    """
+    Check if a given number is prime.
+    
+    Args:
+    num (int): The number to check for primality.
+    
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
     i = 2
     while i ** 2 <= num:
         if num % i == 0:
@@ -8,12 +17,16 @@ def is_prime(num: int):
 
 
 def mersenne_prime(num):
-    """Returns mersenne prime number less than or equal to the given number.
-    mersenne primes are prime numbers of the form 2^n - 1.
-    >>> mersene_prime(10)
-    [3, 7]
-    >>> mersene_prime(100)
-    [3, 7, 31]
+    """
+    Generate a list of Mersenne prime numbers less than or equal to the given number.
+    
+    Mersenne primes are prime numbers of the form 2^n - 1.
+    
+    Args:
+    num (int): The upper limit for generating Mersenne prime numbers.
+    
+    Returns:
+    list: A list of Mersenne prime numbers.
     """
     arr = []
     power = 1
@@ -24,3 +37,9 @@ def mersenne_prime(num):
         power += 1
         value = 2 ** power - 1
     return arr
+
+# Driver code
+if __name__ == "__main__":
+    limit = int(input("Enter a limit: "))
+    mersenne_primes = mersenne_prime(limit)
+    print("Mersenne prime numbers less than or equal to", limit, ":", mersenne_primes)
