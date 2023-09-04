645415a5a484008fd1a88bd0d501486b7875dfb6
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-03 23:34:55 -0700

    Update len_of_list_recursion.py

--- Python Algorithms/len_of_list_recursion.py
+++ Python Algorithms/len_of_list_recursion.py
@@ -1,8 +1,26 @@
-def get_list_lenght(my_list):
-    if my_list == []: # base case
+def get_list_length(my_list):
+    """
+    Recursively calculate the length of a list.
+
+    Arguments:
+    my_list -- the list for which to calculate the length
+
+    Returns:
+    The length of the list.
+    """
+
+    if my_list == []:  # Base case: empty list
         return 0
-    return 1 + get_list_lenght(my_list[1:]) # recursion call
+
+    # Recursive case: add 1 to the length of the list starting from the second element
+    return 1 + get_list_length(my_list[1:])
 
 
-my_user_list = [3, 5, 9, 7, 8, 5, 7] # example of my list
-print(get_list_lenght(my_user_list)) 
+# Driver code to test the get_list_length function
+my_user_list = [3, 5, 9, 7, 8, 5, 7]
+
+# Call the get_list_length function
+length = get_list_length(my_user_list)
+
+# Print the length of the list
+print(f"The length of the list is: {length}.")
