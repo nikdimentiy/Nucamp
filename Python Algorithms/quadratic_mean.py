a996d0f1b956caf008a8a87ab49bebe3a26dba05
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-06 20:49:03 -0700

    Update quadratic_mean.py

--- Python Algorithms/quadratic_mean.py
+++ Python Algorithms/quadratic_mean.py
@@ -1,11 +1,35 @@
-from math import sqrt
+import math
 
 
 def quadratic_mean(array):
     """calculate the root mean square of an array."""
+    """
+    This function calculates the root mean square of an array.
+
+    Args:
+        array: A list of numbers.
+
+    Returns:
+        The root mean square of the array.
+    """
+
     sum = 0
     length = 0
     for i in range(len(array)):
+        # Square each element of the array and add it to the sum.
         sum += array[i] ** 2
+        # Increment the length counter.
         length += 1
-    return sqrt(sum / length)
+
+    # Calculate the mean of the squared elements.
+    mean = sum / length
+
+    # Calculate the root mean square.
+    rms = math.sqrt(mean)
+
+    return rms
+
+
+# Driver code
+array = [1, 2, 3, 4, 5]
+print(quadratic_mean(array))
