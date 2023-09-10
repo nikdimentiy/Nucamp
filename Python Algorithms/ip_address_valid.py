f9f1b640f622f811eeb2c88d5d2f13767a29255e
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-09 19:34:02 -0700

    Update ip_address_valid.py

--- Python Algorithms/ip_address_valid.py
+++ Python Algorithms/ip_address_valid.py
@@ -1,13 +1,23 @@
 import re
 
-# regex for IP address validation IPv4
-
-
 def IP_validation(IP):
+    """
+    Validate an IPv4 address using regular expressions.
+
+    Args:
+        IP (str): The IPv4 address to be validated.
+
+    Returns:
+        bool: True if the IP address is valid, False otherwise.
+    """
+    # Regular expression pattern for IPv4 validation
     regex = r'(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}'
+    
+    # Use re.fullmatch() to match the entire input string against the pattern
     return bool(re.fullmatch(regex, IP))
 
-
+# Example usage
 my_ip_address = '127.0.0.1'
 result = IP_validation(my_ip_address)
-print(result)
+print(f"Is '{my_ip_address}' a valid IPv4 address? {result}")
+
