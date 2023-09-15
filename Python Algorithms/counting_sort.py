2e4ff4d46c84985583ec4cba67c640d19c44c34b
nikdimentiy <nikdimentiy@yahoo.com>
2023-09-14 20:52:30 -0700

    Update counting_sort.py

--- Python Algorithms/counting_sort.py
+++ Python Algorithms/counting_sort.py
@@ -1,17 +1,33 @@
 def counting_sort(arr):
+    """Sorts an array using the counting sort algorithm.
+
+    Args:
+        arr: A list of integers to be sorted.
+
+    Returns:
+        A list of integers sorted in ascending order.
+    """
+
+    # Find the maximum element in the array.
     maxEl = max(arr)
-    countArrayLength = maxEl+1
+
+    # Create a count array to store the number of occurrences of each element in the array.
+    countArrayLength = maxEl + 1
     countArray = [0] * countArrayLength
 
+    # Count the number of occurrences of each element in the array.
     for el in arr:
         countArray[el] += 1
 
+    # Calculate the cumulative sum of the count array.
     for i in range(1, countArrayLength):
-        countArray[i] += countArray[i-1]
+        countArray[i] += countArray[i - 1]
 
+    # Create an output array to store the sorted elements.
     outputArray = [0] * len(arr)
-    i = len(arr) - 1
 
+    # Iterate over the array in reverse order and place each element in its correct position in the output array.
+    i = len(arr) - 1
     while i >= 0:
         currentEl = arr[i]
         countArray[currentEl] -= 1
@@ -20,3 +36,14 @@ def counting_sort(arr):
         i -= 1
 
     return outputArray
+
+
+# Create an unsorted array.
+arr = [5, 3, 2, 1, 4]
+
+# Sort the array using the counting sort algorithm.
+sortedArray = counting_sort(arr)
+
+# Print the sorted array.
+print(sortedArray)
+
