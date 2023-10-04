fa0498a36e67e5733410cd6deaf1eed41258a26c
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-03 23:06:54 -0700

    Update fermats_prime.py

--- Python Algorithms/fermats_prime.py
+++ Python Algorithms/fermats_prime.py
@@ -1,30 +1,36 @@
-# A prime number of the form (2^2^n) + 1 for some non-negative integer n.
-# The first few fermat numbers are 3, 5, 17, 257, 65537, 4294967297, 18446744073709551617
-
 def factors(x):
+    """
+    Find the factors of a given number.
+
+    Args:
+        x (int): The number for which to find factors.
+
+    Returns:
+        list: A list of factors of the input number.
+    """
     factors = []
     i = 2
     s = int(x ** 0.5)
-    while i < s:
+    while i <= s:  # Include the case where i is the square root of x.
         if x % i == 0:
             factors.append(i)
             x = int(x / i)
             s = int(x ** 0.5)
-        i += 1
+        else:
+            i += 1
     factors.append(x)
     return factors
 
-
 print("First 10 Fermat numbers:")
 for i in range(10):
-    fermat = 2 ** 2 ** i + 1
-    print(f"F{chr(i + 0x2080)} = {fermat}")
+    fermat = 2 ** (2 ** i) + 1
+    print(f"F{chr(0x2080 + i)} = {fermat}")
 
-print("\nFactors of first few Fermat numbers:")
+print("\nFactors of the first few Fermat numbers:")
 for i in range(10):
-    fermat = 2 ** 2 ** i + 1
+    fermat = 2 ** (2 ** i) + 1
     fac = factors(fermat)
     if len(fac) == 1:
-        print(f"F{chr(i + 0x2080)} -> IS PRIME")
+        print(f"F{chr(0x2080 + i)} -> IS PRIME")
     else:
-        print(f"F{chr(i + 0x2080)} -> FACTORS: {fac}")
+        print(f"F{chr(0x2080 + i)} -> FACTORS: {fac}")
