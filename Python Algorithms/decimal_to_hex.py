bc51babd0aca7430a152c39e6bf9093c3b898d14
nikdimentiy <nikdimentiy@yahoo.com>
2023-10-03 23:05:18 -0700

    Update decimal_to_hex.py

--- Python Algorithms/decimal_to_hex.py
+++ Python Algorithms/decimal_to_hex.py
@@ -1,15 +1,29 @@
-# convert decimal numbers to hexadecimal numbers
 def decimal_to_hexadecimal(decimal_number):
-    hexadecimal_number = ''
+    """
+    Convert a decimal number to a hexadecimal number.
+
+    Args:
+        decimal_number (int): The decimal number to be converted.
+
+    Returns:
+        str: The hexadecimal representation of the decimal number.
+    """
+    hexadecimal_number = ''  # Initialize an empty string to store the hexadecimal representation.
+
+    # Define a list of hexadecimal digits.
     hexadecimal_number_list = ['0', '1', '2', '3', '4',
                                '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+    # Perform the conversion using repeated division by 16.
     while decimal_number > 0:
-        hexadecimal_number = hexadecimal_number_list[decimal_number %
-                                                     16] + hexadecimal_number
-        decimal_number = decimal_number // 16
+        # Append the remainder (hexadecimal digit) to the left of the current hexadecimal representation.
+        hexadecimal_number = hexadecimal_number_list[decimal_number % 16] + hexadecimal_number
+        decimal_number = decimal_number // 16  # Perform integer division by 16.
+
     return hexadecimal_number
 
-
-
-result = decimal_to_hexadecimal(255)
-print(result)
+# Driver code to test the function
+if __name__ == "__main__":
+    decimal_input = 255
+    hexadecimal_result = decimal_to_hexadecimal(decimal_input)
+    print(f"The hexadecimal representation of {decimal_input} is: {hexadecimal_result}")
