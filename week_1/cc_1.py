c4ec5f3bb3275d0e0f6d139d4fb3075ad89ddb30
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-03-05 23:40:39 -0800

    Added comments and improved code readability

--- week_1/cc_1.py
+++ week_1/cc_1.py
@@ -1,15 +1,22 @@
-# Turn this line into a comment
-
+# List of cities
 cities = ["Seattle", "Tacoma", "Bellevue"]
 
+# Assigning integer value to variable 'a'
 a = 1
+# Printing type of 'a'
 print(type(a))
 
+# Assigning float value to variable 'b'
 b = 2.5
+# Printing type of 'b'
 print(type(b))
 
+# Assigning string value to variable 'c'
 c = "Nucamp"
+# Printing type of 'c'
 print(type(c))
 
+# Adding variables 'a' and 'b' and assigning the result to variable 'd'
 d = a + b
+# Printing the result of addition
 print(d)
