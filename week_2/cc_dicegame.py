90dd3e91cb88be7541ca8e240f76574fc4243787
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-03-05 23:41:54 -0800

    Added docstring, comments, and improved code structure for better readability in the dice game script.

--- week_2/cc_dicegame.py
+++ week_2/cc_dicegame.py
@@ -1,11 +1,15 @@
 import random
 
-# initialize a high score
+# Initialize a high score
 high_score = 0
 
-
-# define function
 def dice_game():
+    """
+    Simulates a simple dice game where the player can roll two dice and attempt to beat the high score.
+
+    Returns:
+        None
+    """
     global high_score
     while True:
         print("Current High Score:", high_score)
@@ -14,6 +18,7 @@ def dice_game():
         user_choice = (input("Enter your choice: "))
         print("\n")
         if user_choice == "1":
+            # Roll two dice
             a = random.randint(1, 6)
             b = random.randint(1, 6)
             print(f"You roll a...{a}")
@@ -21,6 +26,7 @@ def dice_game():
             print("\n")
             new_score = a + b
             print(f"You have rolled a total of: {new_score} \n")
+            # Check if the new score beats the high score
             if new_score > high_score:
                 high_score = new_score
                 print("New High Score!\n")
@@ -29,7 +35,6 @@ def dice_game():
             break
         else:
             print("Unknown Input! Only two choices: 1 <--||--> 2: ")
-
 
 # Run function
 dice_game()
