908b66608f2a20c714dfe0a6eaec77a7aa16a877
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-03-08 20:33:29 -0800

    Added function to reverse user input string with comments and docstring for clarity

--- week_2/cc_reverse.py
+++ week_2/cc_reverse.py
@@ -1,6 +1,18 @@
 def reverse(name):
+    """
+    Reverses the given string.
+
+    Args:
+    - name (str): The string to be reversed.
+
+    Returns:
+    - str: The reversed string.
+    """
+    # Using slicing with [::-1] to reverse the string
     return name[::-1]
 
-
+# Ask for user input
 name = input("What is your name? ")
+
+# Call the reverse function and print the result
 print("Your name reversed is:", reverse(name))
