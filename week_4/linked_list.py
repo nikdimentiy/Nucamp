bea5894983dd6e7deb56cd11b10199ba189e290f
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-03-10 22:21:47 -0700

    Implemented prepend method in LinkedList class with proper node linking. Updated comments and improved code readability.

--- week_4/linked_list.py
+++ week_4/linked_list.py
@@ -1,6 +1,3 @@
-#  Code Challenge: Linked List Prepend Method
-
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -12,35 +9,45 @@ class LinkedList:
         self.head = None
 
     def prepend(self, value):
+        """Add a new node with the given value at the beginning of the linked list."""
         new_node = Node(value)
 
         if self.head is None:
+            # If the list is empty, set the new node as the head
             self.head = new_node
             print("Head Node created:", self.head.value)
             return
 
-        node = self.head
-        node.next = node
+        # If the list is not empty, set the new node as the head and link it to the previous head
+        new_node.next = self.head
         self.head = new_node
         print("Prepended new Head Node with value:", self.head.value)
-        print("Node following head is:", node.next.value)
+        if self.head.next:
+            print("Node following head is:", self.head.next.value)
+        else:
+            print("No node following head.")
 
     def append(self, value):
+        """Add a new node with the given value at the end of the linked list."""
         new_node = Node(value)
 
         if self.head is None:
+            # If the list is empty, set the new node as the head
             self.head = new_node
             print("Head Node created:", self.head.value)
             return
 
+        # Traverse the list to find the last node
         node = self.head
         while node.next is not None:
             node = node.next
 
+        # Set the new node as the next node of the last node
         node.next = new_node
         print("Appended new Node with value:", node.next.value)
 
 
+# Test the LinkedList class
 llist = LinkedList()
 llist.prepend("First Node")
 llist.prepend("Inserted New First Node")
