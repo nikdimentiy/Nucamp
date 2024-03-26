5d597f4ff218a6b5fc579f6a601e3f7a355ed60e
Dmytro Nikey <nikdimentiy@yahoo.com>
2024-03-25 23:03:32 -0700

    Refactored SQLAlchemy models for better organization and clarity. Added comments for improved code understanding. Ready for review and merge

--- SQL/week3_models.py
+++ SQL/week3_models.py
@@ -3,55 +3,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Reference:
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
-# https://docs.sqlalchemy.org/en/14/core/metadata.html#sqlalchemy.schema.Column
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/#many-to-many-relationships
-
-
+# Define the association table for the many-to-many relationship between users and tweets
 likes_table = db.Table(
     'likes',
-    db.Column(
-        'user_id', db.Integer,
-        db.ForeignKey('users.id'),
-        primary_key=True
-    ),
-
-    db.Column(
-        'tweet_id', db.Integer,
-        db.ForeignKey('tweets.id'),
-        primary_key=True
-    ),
-
-    db.Column(
-        'created_at', db.DateTime,
-        default=datetime.datetime.utcnow,
-        nullable=False
-    )
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('tweet_id', db.Integer, db.ForeignKey('tweets.id'), primary_key=True),
+    db.Column('created_at', db.DateTime, default=datetime.datetime.utcnow, nullable=False)
 )
 
 
 class User(db.Model):
     __tablename__ = 'users'
+
+    # Define columns for the User table
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
-    # get all tweets for given user
+
+    # Define relationship with tweets posted by the user
     tweets = db.relationship('Tweet', backref='user', cascade="all,delete")
-    # get all tweets a user liked
-    ltweets = db.relationship(
+
+    # Define relationship with tweets the user has liked
+    liked_tweets = db.relationship(
         'Tweet', secondary=likes_table,
         lazy='subquery',
-        backref=db.backref('liked_tweets', lazy=True)
+        backref=db.backref('liked_by_users', lazy=True)
     )
 
-    # for creating instance
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
 
-    # can design how you like - good to have as method of class to user later on w/o needing to write logic inside endpoint
     def serialize(self):
+        """Serialize User object to JSON"""
         return {
             'id': self.id,
             'username': self.username
@@ -60,17 +44,17 @@ class User(db.Model):
 
 class Tweet(db.Model):
     __tablename__ = 'tweets'
+
+    # Define columns for the Tweet table
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.String(280), nullable=False)
-    created_at = db.Column(
-        db.DateTime,
-        default=datetime.datetime.utcnow,
-        nullable=False
-    )
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
 
+    # Define relationship with the user who posted the tweet
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    # get all users who like a given tweet
-    likes = db.relationship(
+
+    # Define relationship with users who have liked the tweet
+    liked_by_users = db.relationship(
         'User', secondary=likes_table,
         lazy='subquery',
         backref=db.backref('liked_tweets', lazy=True)
@@ -81,6 +65,7 @@ class Tweet(db.Model):
         self.user_id = user_id
 
     def serialize(self):
+        """Serialize Tweet object to JSON"""
         return {
             'id': self.id,
             'content': self.content,
